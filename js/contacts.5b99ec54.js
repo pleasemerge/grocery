9c3c47642cbf4a3ee02512302671a11d01b89f91
Vladimir Isakov <gvncode@gmail.com>
2022-11-17 17:54:17 +0300

    Build

---
+++ js/contacts.5b99ec54.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgrocery"]=self["webpackChunkgrocery"]||[]).push([[421],{4248:function(r,n,e){e.r(n),e.d(n,{default:function(){return f}});var t=e(6252);function u(r,n,e,u,c,a){return(0,t.wg)(),(0,t.iD)("div",null," Contact ")}var c={},a=e(3744);const o=(0,a.Z)(c,[["render",u]]);var f=o}}]);
+//# sourceMappingURL=contacts.5b99ec54.js.map
